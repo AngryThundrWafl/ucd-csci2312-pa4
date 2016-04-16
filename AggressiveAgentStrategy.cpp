e83f2fdd957a00c4e060b4ef77d1d1be8a9bee56
Brian <pinabrian2015@gmail.com>
2016-04-16 17:40:39 -0600

    started Agressive agent strategy

--- AggressiveAgentStrategy.cpp
+++ AggressiveAgentStrategy.cpp
@@ -8,5 +8,20 @@ namespace  Gaming{
     //Provided from PA4 read me
     const double AggressiveAgentStrategy::DEFAULT_AGGRESSION_THRESHOLD = Game::STARTING_AGENT_ENERGY * 0.75;
 
+    AggressiveAgentStrategy::AggressiveAgentStrategy(double agentEnergy) {
+        __agentEnergy = agentEnergy;
+    }
+
+    AggressiveAgentStrategy::~AggressiveAgentStrategy() {
+        //Todo maybe
+    }
+    //strategy for agrressive agent
+    ActionType AggressiveAgentStrategy::operator()(const Surroundings &s) const {
+        //TODO implement so that the agent finds any means of play using if sttments
+
+
+
+        return STAY;        // done last if no condtitioins meet the agressive agents startegy
+    }
 }
 
