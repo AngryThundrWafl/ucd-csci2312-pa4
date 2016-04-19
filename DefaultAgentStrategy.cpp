9d5eb10a4b42aff3ff4ff8655eb0eaecb9298082
Brian <pinabrian2015@gmail.com>
2016-04-18 20:51:29 -0600

    switch statement is unreachable???

--- DefaultAgentStrategy.cpp
+++ DefaultAgentStrategy.cpp
@@ -3,7 +3,7 @@
 //
 #include "DefaultAgentStrategy.h"
 #include "Game.h"
-#include <vector>
+#include <ctime>
 
 namespace Gaming{
 
@@ -21,6 +21,7 @@ namespace Gaming{
         PositionRandomizer random;
         Position p, mid(1,1);
         std::vector<int> ways;
+        int location;                                       //will hold the location of the piece
 
         //loop to check if theres any pieces
         for(int i = 0; i < 8; i++){
@@ -37,6 +38,7 @@ namespace Gaming{
                 surround[3] = true;     //checks if theres a simple piece
             }
         }
+        std::default_random_engine gen(time(0));
         //todo if statements for each instance of bool surround
         if (surround[0]) {
             //advatage is prioity
@@ -44,9 +46,7 @@ namespace Gaming{
                 if (s.array[i] == ADVANTAGE)
                     ways.push_back(i);
             }
-
-            p = random(ways);
-            return Game::reachSurroundings(mid, p);
+            location = ways[gen() % ways.size()];
         }
             //if there is not advantage it will check for food
         else if (surround[1]) {
@@ -54,9 +54,7 @@ namespace Gaming{
                 if (s.array[i] == FOOD)
                     ways.push_back(i);
             }
-
-            p = random(ways);
-            return Game::reachSurroundings(mid, p);
+            location = ways[gen() % ways.size()];
         }
             //if no food will check for Empty space to move to
         else if (surround[2]) {
@@ -64,9 +62,7 @@ namespace Gaming{
                 if (s.array[i] == EMPTY)
                     ways.push_back(i);
             }
-
-            p = random(ways);
-            return Game::reachSurroundings(mid, p);
+            location = ways[gen() % ways.size()];
         }
             //if there is no empty space will check for a simple agent to attack
         else if (surround[3]) {
@@ -74,14 +70,50 @@ namespace Gaming{
                 if (s.array[i] == SIMPLE)
                     ways.push_back(i);
             }
-
-            p = random(ways);
-            return Game::reachSurroundings(mid, p);
+            location = ways[gen() % ways.size()];
         }
 
         //if surround returns all false it is unable to remove so it will return stay
         return STAY;
         //todo include a switch statment for each movement case
+        switch(location){       //each case will be for each part of the surroundings in the array
+            case 1:
+                p.x = mid.x -1;
+                p.y = mid.y -1;
+                break;
+            case 2:
+                p.x = mid.x;
+                p.y = mid.y;
+                break;
+            case 3:
+                p.x = mid.x;
+                p.y = mid.y;
+                break;
+            case 4:
+                p.x = mid.x;
+                p.y = mid.y;
+                break;
+            case 5:
+                p.x = mid.x;
+                p.y = mid.y;
+                break;
+            case 6:
+                p.x = mid.x;
+                p.y = mid.y;
+                break;
+            case 7:
+                p.x = mid.x;
+                p.y = mid.y;
+                break;
+            case 8:
+                p.x = mid.x;
+                p.y = mid.y;
+                break;
+            case 9:
+                p.x = mid.x;
+                p.y = mid.y;
+                break;
+        }
     }
 
 }
